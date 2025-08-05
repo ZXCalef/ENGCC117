1a0739ef1beb7cd3765d13cce1b639b70d206c00
witwisit jitwilai <tjitwilai@gmail.com>
2025-08-05 19:51:47 +0700

    update

---
+++ Lab06/Lab06 For-Loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+    int N ;
+    printf( "Enter value: " ) ;
+    scanf( "%d" , &N ) ;
+
+    if (N % 2 == 1) { // เลขคี่
+        printf( "Series: " , N ) ;
+        for ( int i = 1 ; i <= N ; i += 2 ) {
+            printf( "%d ", i ) ;
+        }// end for
+        
+    } else { // เลขคู่
+        printf( "Series: ", N ) ;
+        for ( int i = N ; i >= 0 ; i -= 2 ) {
+            printf( "%d ", i ) ;
+        }// end for
+    }// end if
+
+    return 0 ;
+}//end function
