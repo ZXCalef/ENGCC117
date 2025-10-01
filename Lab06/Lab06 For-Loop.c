761422527515b47aaa40a150b893b6c7034c35c7
witwisit jitwilai <tjitwilai@gmail.com>
2025-10-01 21:37:34 +0700

    update

--- Lab06/Lab06 For-Loop.c
+++ Lab06/Lab06 For-Loop.c
@@ -5,18 +5,18 @@ int main () {
     printf( "Enter value: " ) ;
     scanf( "%d" , &N ) ;
 
-    if (N % 2 == 1) { // เลขคี่
+    if (N % 2 == 1) {
         printf( "Series: " , N ) ;
         for ( int i = 1 ; i <= N ; i += 2 ) {
             printf( "%d ", i ) ;
-        }// end for
+        }
         
     } else { // เลขคู่
         printf( "Series: ", N ) ;
         for ( int i = N ; i >= 0 ; i -= 2 ) {
             printf( "%d ", i ) ;
-        }// end for
-    }// end if
+        }
+    }
 
     return 0 ;
-}//end function
+}
