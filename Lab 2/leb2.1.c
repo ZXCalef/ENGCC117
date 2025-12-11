4dd1334f512a31e1a76804f56072627ebd792346
witwisit jitwilai <tjitwilai@gmail.com>
2025-12-11 21:37:25 +0700

    update

--- Lab 2/leb2.1.c
+++ Lab 2/leb2.1.c
@@ -7,26 +7,29 @@ int main() {
     char text[50];
     char out[50];
     
-    printf("Enter text: ");
-    fgets(text, sizeof(text), stdin);
+    printf("Enter string: ");
+    fgets(text, 50, stdin);
     
-    text[strcspn(text, "\n")] = '\0';
+    // ลบ newline ที่ติดมาจาก fgets
+    int len = strlen(text);
+    if (len > 0 && text[len-1] == '\n') {
+        text[len-1] = '\0';
+    }
     
     reverse(text, out);
-    
-    printf("\nInput: %s\n", text);
-    printf("Output: %s\n", out);
+    printf("Original: %s\n", text);
+    printf("Reversed: %s\n", out);
     
     return 0;
 }
 
 void reverse(char str1[], char str2[]) {
-    int len = strlen(str1);
+    int length = strlen(str1);
     int i;
     
-    for (i = 0; i < len; i++) {
-        str2[i] = str1[len - 1 - i];
+    for (i = 0; i < length; i++) {
+        str2[i] = str1[length - 1 - i];
     }
     
-    str2[i] = '\0';
+    str2[length] = '\0';
 }
