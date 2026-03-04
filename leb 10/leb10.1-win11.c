77412dd39163fc645b1ccbfbdd9a704b2ccdde12
witwisit jitwilai <tjitwilai@gmail.com>
2026-03-04 17:12:30 +0700

    up

--- leb 10/leb10.1-win11.c
+++ leb 10/leb10.1-win11.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
+long SumLoop(int n);
+long SumRecur(int n);
+
+int main() {
+    int n = 10;
+
+    printf("SumLoop(%d) = %ld\n", n, SumLoop(n));
+    printf("SumRecur(%d) = %ld\n", n, SumRecur(n));
+
+    return 0;
+}
+
 long SumLoop(int n) {
+    if (n <= 0)
+        return 0;
+
     long sum = 0;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
         sum += i;
-    }
+
     return sum;
 }
 
 long SumRecur(int n) {
-    if (n <= 1) {
-        return n;
-    }
-    return n + SumRecur(n - 1);
-}
+    if (n <= 0)
+        return 0;
 
-int main() {
-    printf("SumLoop(10) = %ld\n", SumLoop(10));
-    printf("SumRecur(10) = %ld\n", SumRecur(10));
-    
-    printf("\n--- Testing with large n values ---\n");
-    
-    printf("SumLoop(1000000) = %ld\n", SumLoop(1000000));
-    printf("SumLoop(10000000) = %ld\n", SumLoop(10000000));
-    printf("SumLoop(100000000) = %ld\n", SumLoop(100000000));
-    
-    printf("SumRecur(10000) = %ld\n", SumRecur(10000));
-    
-    return 0;
+    return n + SumRecur(n - 1);
 }
