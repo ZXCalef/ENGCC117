f443c0c2d53c8d5d5abbbd7eaf065d8d99ebd3df
witwisit jitwilai <tjitwilai@gmail.com>
2026-03-04 15:33:33 +0700

    up

--- leb 10/leb10.1-win11.c
+++ leb 10/leb10.1-win11.c
@@ -19,17 +19,13 @@ int main() {
     printf("SumLoop(10) = %ld\n", SumLoop(10));
     printf("SumRecur(10) = %ld\n", SumRecur(10));
     
-    // ทดลองใช้ค่า n สูงๆ เพื่อให้เกิด run-time error
     printf("\n--- Testing with large n values ---\n");
     
-    // ทดสอบ SumLoop กับค่าสูง
     printf("SumLoop(1000000) = %ld\n", SumLoop(1000000));
     printf("SumLoop(10000000) = %ld\n", SumLoop(10000000));
     printf("SumLoop(100000000) = %ld\n", SumLoop(100000000));
     
-    // ทดสอบ SumRecur กับค่าสูง (จะเกิด stack overflow)
     printf("SumRecur(10000) = %ld\n", SumRecur(10000));
-    // printf("SumRecur(100000) = %ld\n", SumRecur(100000)); // จะเกิด stack overflow
     
     return 0;
 }
--- leb 10/leb10.2-win11.c
+++ leb 10/leb10.2-win11.c
@@ -4,10 +4,10 @@ void TowerHanoi(int m, int i, int j) {
     if (m == 1) {
         printf("Disc %d from %d to %d\n", m, i, j);
     } else {
-        int k = 6 - i - j;  // หาเสากลาง (1+2+3 = 6)
-        TowerHanoi(m - 1, i, k);  // ย้าย m-1 แผ่นจาก i ไป k
-        printf("Disc %d from %d to %d\n", m, i, j);  // ย้ายแผ่นล่างสุด
-        TowerHanoi(m - 1, k, j);  // ย้าย m-1 แผ่นจาก k ไป j
+        int k = 6 - i - j;
+        TowerHanoi(m - 1, i, k);
+        printf("Disc %d from %d to %d\n", m, i, j);
+        TowerHanoi(m - 1, k, j);
     }
 }
 
