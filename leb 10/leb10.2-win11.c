91b9b8bd9bfce570315fad88acbe0a2397dc1ad8
witwisit jitwilai <tjitwilai@gmail.com>
2026-03-04 17:15:49 +0700

    up

--- leb 10/leb10.2-win11.c
+++ leb 10/leb10.2-win11.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
-void TowerHanoi(int m, int i, int j) {
-    if (m == 1) {
-        printf("Disc %d from %d to %d\n", m, i, j);
-    } else {
-        int k = 6 - i - j;
-        TowerHanoi(m - 1, i, k);
-        printf("Disc %d from %d to %d\n", m, i, j);
-        TowerHanoi(m - 1, k, j);
-    }
-}
+void TowerHanoi(int n, int from, int to);
 
 int main() {
-    TowerHanoi(3, 1, 3);
+    int n = 3;
+    TowerHanoi(n, 1, 3);
     return 0;
+}
+
+void TowerHanoi(int n, int from, int to) {
+    if (n == 1) {
+        printf("Disc 1 from %d to %d\n", from, to);
+        return;
+    }
+    
+    int aux;
+    if (from == 1 && to == 2) aux = 3;
+    else if (from == 1 && to == 3) aux = 2;
+    else if (from == 2 && to == 1) aux = 3;
+    else if (from == 2 && to == 3) aux = 1;
+    else if (from == 3 && to == 1) aux = 2;
+    else if (from == 3 && to == 2) aux = 1;
+    
+    TowerHanoi(n - 1, from, aux);
+    printf("Disc %d from %d to %d\n", n, from, to);
+    TowerHanoi(n - 1, aux, to);
 }
