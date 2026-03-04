1cd370934ce60abcd7135380c18700c0633cfdc3
witwisit jitwilai <tjitwilai@gmail.com>
2026-03-04 17:21:47 +0700

    up

--- leb 11/leb11.1.c
+++ leb 11/leb11.1.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 
-int BinSearch(int data[], int n, int find) {
-    int low = 0;
-    int high = n - 1;
-    int mid;
-    
-    while (low <= high) {
-        mid = (low + high) / 2;
-        
-        if (data[mid] == find) {
-            return mid;
-        }
-        else if (data[mid] < find) {
-            low = mid + 1;
-        }
-        else {
-            high = mid - 1;
-        }
-    }
-    
-    return -1;
-}
+int BinSearch(int data[], int n, int find);
 
 int main() {
     int data[6] = {1, 2, 3, 4, 5, 7};
-    int n = 6;
-    int find = 5;
-    
+    int n = 6, find = 5;
     int pos = BinSearch(data, n, find);
     
-    if (pos != -1) {
-        printf("Found %d at position %d\n", find, pos);
-    } else {
-        printf("Not found\n");
-    }
+    printf("Found %d at position %d\n", find, pos);
     
     return 0;
+}
+
+int BinSearch(int data[], int n, int find) {
+    int left = 0;
+    int right = n - 1;
+    
+    while (left < right) {
+        int mid = (left + right) / 2;
+        
+        if (data[mid] < find)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    
+    if (data[left] == find)
+        return left;
+    return -1;
 }
